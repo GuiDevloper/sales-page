56d60a34d01a11a0997db09349c4f96039b04bd1
GuiDevloper <guicorrea384@gmail.com>
2024-11-08 01:25:06 +0000

    Deploying to master from @ GuiDevloper/sales-page@1afaa86d407e8ed30b566713dc7d66cb3bd92162 🚀

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ self.context = {
     "development": false,
     "production": true,
     "mode": "ssg",
-    "key": "62a601919ce81b7e9e385cf0751738bacc737d40",
+    "key": "d9e597ddd44784497af07c7d270518d1ca26f9fb",
     "name": ""
   },
   "project": {
@@ -35,7 +35,7 @@ self.context = {
   },
   "settings": {},
   "worker": {
-    "enabled": true,
+    "enabled": false,
     "fetching": false,
     "preload": [],
     "staleWhileRevalidate": [],
